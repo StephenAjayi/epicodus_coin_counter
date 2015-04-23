810250e515d1094f84daee350d5354e77aba028d
Franz Knupfer & Stephen Ajayi <student@epicodus.com>
2015-04-23 15:34:17 -0700

    working version of program

--- lib/coin_counter.rb
+++ lib/coin_counter.rb
@@ -1,13 +1,36 @@
-require('rspec')
-require('coin_counter')
+class Fixnum
+  define_method(:coin_counter) do
+    penny = 1
+    nickel = 5
+    dime = 10
+    quarter = 25
+    num_penny = 0
+    num_nickel = 0
+    num_dime = 0
+    num_quarter =0
+    change_owed = self
+    change = []
 
-describe('Float#coin_counter') do
-  it("will count pennies up to .99") do
-    expect(.99).coin_counter())to(eq(99))
+    until change_owed.<(25)
+      change_owed = change_owed.-(quarter)
+      num_quarter = num_quarter.+(1)
+    end
+    change.push(num_quarter)
+    until change_owed.<(10)
+      change_owed= change_owed.-(dime)
+      num_dime = num_dime.+(1)
+    end
+    change.push(num_dime)
+    until change_owed.<(5)
+      change_owed =  change_owed.-(nickel)
+      num_nickel = num_nickel.+(1)
+    end
+    change.push(num_nickel)
+    until change_owed.==(0)
+      change_owed = change_owed.-(penny)
+      num_penny = num_penny.+(1)
+    end
+    change.push(num_penny)
+    change
   end
-  it("will return a string that says the number of pennies")
-    expect(.99().coin_counter()).to(eq("99 pennies"))
-  end
-  # it("will count nickels and pennies up to .99 using the smallest number of coins") do
-  #   expect(.99).coin_counter())to(eq("19 nickels and 4 pennies"))
 end
--- spec/coin_counter_spec.rb
+++ spec/coin_counter_spec.rb
@@ -1,10 +1,18 @@
-class Float
-  define_method(:coin_counter) do
-    change = 0
-    penny = 0.01
+require('rspec')
+require('coin_counter')
+require('pry')
 
-    until change.==(self)
-      change= change.+(penny)
-    end
+describe('Fixnum#coin_counter') do
+  it("will count pennies up to four cents") do
+    expect((4).coin_counter()).to(eq([0,0,0,4]))
   end
+it("will count nickels and pennies up to nine cents") do
+   expect((9).coin_counter()).to(eq([0,0,1,4]))
+   end
+   it("will return the least amount of coins using quarters, dimes, nickels and cents") do
+     expect((99).coin_counter()).to(eq([3,2,0,4]))
+   end
 end
+# # it("will count nickels and pennies up to 99 using the smallest number of coins") do
+# #   expect(99).coin_counter())to(eq("19 nickels and 4 pennies"))
+# end
